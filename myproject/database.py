b0928b1ffefd30ac957013433eec417b29f58178
JurnWynants <r0931107@student.thomasmore.be>
2023-11-09 09:45:32 +0100

    fkdsj

--- myproject/database.py
+++ myproject/database.py
@@ -2,12 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://Jurn:test@localhost:5432/APIdb"
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./sqlitedb/sqlitedata.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://Jurn:test@0.0.0.0:5432/APIdb"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, echo=True, connect_args={"check_same_thread": False},
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
